439d5554cb7a31700266fe6b221fb5dea7db7e72
The-Renaissance <nashesvobodnoye@hotmail.com>
2017-10-28 13:59:19 -0400

    Initial setup
    Moving my Atmel project in

---
+++ Rocket library/Rocket library/main.c
@@ -0,0 +1,19 @@
+/*
+ * Rocket library.c
+ *
+ * Created: 2017/10/24 23:53:59
+ * Author : Lenovo
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+		
+    }
+}
+
