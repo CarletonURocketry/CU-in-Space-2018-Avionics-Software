e985a893f278ac9a7fc271a1b1657ef80d268f10
Samuel Dewan <samdewan@me.com>
2017-10-31 17:50:27 -0400

    Added ADC.c and ADC.h

---
+++ CU-in-Space-2018-Avionics-Software/ADC.c
@@ -0,0 +1,8 @@
+//
+//  ADC.c
+//  CU-in-Space-2018-Avionics-Software
+//
+//  Created by Samuel Dewan on 2017-10-31.
+//
+
+#include "ADC.h"
---
+++ CU-in-Space-2018-Avionics-Software/ADC.h
@@ -0,0 +1,14 @@
+//
+//  ADC.h
+//  CU-in-Space-2018-Avionics-Software
+//
+//  Created by Samuel Dewan on 2017-10-31.
+//
+//  Asynchronously read data from analog inputs
+//
+
+#ifndef ADC_h
+#define ADC_h
+
+
+#endif /* ADC_h */
