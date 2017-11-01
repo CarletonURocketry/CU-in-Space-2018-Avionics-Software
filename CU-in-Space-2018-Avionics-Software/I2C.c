b68feea3ece3120eb6ab6b75551eca8a2f63192f
Samuel Dewan <samdewan@me.com>
2017-11-01 19:10:40 -0400

    Wrote I2C.h and stubs of I2C.c

--- CU-in-Space-2018-Avionics-Software/I2C.c
+++ CU-in-Space-2018-Avionics-Software/I2C.c
@@ -6,3 +6,150 @@
 //
 
 #include "I2C.h"
+
+#include <avr/io.h>
+
+// MARK: Constants
+#define I2C_RX_BUFFER_LENGTH    256
+
+#define I2C_WRITE_BIT   0
+#define I2C_READ_BIT    1
+
+#define I2C_MAX_ERRORS  3     // Number of errors before an I2C interaction is aborted
+
+#define I2C_FLAGS_ACTIVE            0
+#define I2C_FLAGS_SUCCESSFUL        1
+
+// MARK: Variable Definitions
+static char i2c_rx_buffer[I2C_RX_BUFFER_LENGTH];
+static volatile uint8_t i2c_rx_insert_point;
+static volatile uint8_t i2c_rx_withdraw_point;
+
+static uint8_t *i2c_tx_buffer;
+static uint16_t i2c_short_tx_data_buffer;
+
+static volatile uint8_t i2c_address;
+static volatile uint8_t i2c_reg;
+static volatile uint8_t i2c_tx_length;
+static volatile uint8_t i2c_rx_length;
+static volatile uint8_t i2c_position;
+static volatile uint8_t i2c_error_count;
+static volatile uint8_t i2c_flags;
+
+// MARK: Function Definitions
+void init_i2c(void)
+{
+    
+}
+
+void i2c_service(void)
+{
+    
+}
+
+uint8_t i2c_transfer_active(void)
+{
+    return 1;
+}
+
+uint8_t i2c_has_byte (void)
+{
+    return 0;
+}
+
+uint8_t i2c_get_byte (void)
+{
+    return 0;
+}
+
+uint16_t i2c_get_word (void)
+{
+    return 0;
+}
+
+uint32_t i2c_get_long_word(void)
+{
+    return 0;
+}
+
+
+void i2c_write_byte(uint8_t address, uint8_t reg, uint8_t data)
+{
+    
+}
+
+void i2c_write_word(uint8_t address, uint8_t reg, uint16_t data)
+{
+    
+}
+
+void i2c_write_block(uint8_t address, uint8_t reg, uint8_t *data, uint8_t length)
+{
+    
+}
+
+
+void i2c_read_byte(uint8_t address, uint8_t reg)
+{
+    
+}
+
+void i2c_read_word(uint8_t address, uint8_t reg)
+{
+    
+}
+
+void i2c_read_block(uint8_t address, uint8_t reg, uint8_t length)
+{
+    
+}
+
+// MARK: Interupt service routines
+ISR (TWI_vect)
+{
+    switch (TWSR & 0x03) {
+        case 0x08:  // Transmition of start condition has finished
+            // Transmit adress + W
+            break;
+        case 0x10:  // Transmition of repeated start has finished
+            // Transmit adress + R
+            break;
+        case 0x18:  // Address + W has been sent, ACK recieved
+            // Transmit Command
+            break;
+        case 0x20:  // Address + W has been sent, NOT ACK recieved
+            // Increment error count and restart transaction or abort as appropriate
+            // Restart transaction
+            break;
+        case 0x30:  // Data byte had been transmitted, NOT ACK recieved
+            // Increment error count and restart transaction or abort as appropriate
+            
+            // Try to resend byte
+            // Set up to resend command or decrement position as appropriate
+
+            // Fall through to resend bytes
+        case 0x28:  // Data byte had been transmitted, ACK recieved
+            // Send next byte as appropriate
+
+            break;
+        case 0x38:  // Lost arbitration
+            // Increment error count and restart transaction or abort as appropriate
+            break;
+        case 0x40:  // Address + R has been sent, ACK recieved
+            // Reset possition
+            // Receive bytes and send ACK or NACK as appropriate
+            break;
+        case 0x48:  // Address + R has been sent, NOT ACK recieved
+            // Increment error count and resend address or abort as appropriate
+            break;
+        case 0x50:  // Data byte recieved, ACK sent
+            // Get ready to receive next byte
+            break;
+        case 0x58:  // Data byte recieved, NOT ACK sent
+            // Read finished, wrap up transaction
+            break;
+        default:
+            // Should never be reached
+            break;
+    }
+}
--- CU-in-Space-2018-Avionics-Software/I2C.h
+++ CU-in-Space-2018-Avionics-Software/I2C.h
@@ -8,6 +8,98 @@
 #ifndef I2C_h
 #define I2C_h
 
-#include <stdio.h>
+#include "global.h"
+
+/**
+ *  Initialize the TWI interface in fast mode (400kHz)
+ */
+extern void init_i2c(void);
+
+/**
+ *  Service to be run in each iteration of the main loop
+ */
+extern void i2c_service(void);
+
+/**
+ *  Determine if there is a transfer in progress on the i2c bus
+ *  @returns 1 if there is an active transfer on the i2c bus, otherwise returns 0
+ */
+extern uint8_t i2c_transfer_active(void);
+
+
+/**
+ *  Determine if there is a byte avaliable to be read from the i2c bus RX buffer
+ *  @returns 0 if there is no byte avaliable, a positive integer otherwise
+ */
+extern uint8_t i2c_has_byte (void);
+
+/**
+ *  Get the byte least recently placed in the i2c bus RX buffer
+ *  @returns The least recently recieved byte in the i2c bus RX buffer, or 0 if the buffer is empty
+ */
+extern uint8_t i2c_get_byte (void);
+
+/**
+ *  Get the 2 bytes least recently placed in the i2c bus RX buffer
+ *  @returns The least recently recieved word in the i2c bus RX buffer, or 0 if the buffer is empty
+ */
+extern uint16_t i2c_get_word (void);
+
+/**
+ *  Get the 4 bytes least recently placed in the i2c bus RX buffer
+ *  @returns The least recently recieved long word in the i2c bus RX buffer, or 0 if the buffer is empty
+ */
+extern uint32_t i2c_get_long_word(void);
+
+
+/**
+ *  Writes a byte to a register via the i2c bus
+ *  @param address The address of the slave to which the byte should be sent
+ *  @param reg The address of the register to which the data should be written
+ *  @param data The byte to be sent
+ */
+extern void i2c_write_byte(uint8_t address, uint8_t reg, uint8_t data);
+
+/**
+ *  Writes a word to a register via the i2c bus
+ *  @param address The address of the slave to which the byte should be sent
+ *  @param reg The address of the register to which the data should be written
+ *  @param data The word to be sent
+ */
+extern void i2c_write_word(uint8_t address, uint8_t reg, uint16_t data);
+
+/**
+ *  Writes a block to a register via the i2c bus
+ *  @param address The address of the slave to which the byte should be sent
+ *  @param reg The address of the register to which the data should be written
+ *  @param data An array of bytes to be sent
+ *  @param length The number of bytes to be sent
+ */
+extern void i2c_write_block(uint8_t address, uint8_t reg, uint8_t *data, uint8_t length);
+
+/**
+ *  Read a one byte wide register via the i2c bus
+ *  The received byte will be written to the i2c bus RX buffer
+ *  @param address The address of the slave with which the transfer should be completed
+ *  @param reg The address of the register which should be read
+ */
+extern void i2c_read_byte(uint8_t address, uint8_t reg);
+
+/**
+ *  Read one word wide a register via the i2c bus
+ *  The received byte will be written to the i2c bus RX buffer
+ *  @param address The address of the slave with which the transfer should be completed
+ *  @param reg The address of the register which should be read
+ */
+extern void i2c_read_word(uint8_t address, uint8_t reg);
+
+/**
+ *  Read a register via the i2c bus
+ *  The received byte will be written to the i2c bus RX buffer
+ *  @param address The address of the slave with which the transfer should be completed
+ *  @param reg The address of the register which should be read
+ *  @param length The number of bytes to be read from the peripheral
+ */
+extern void i2c_read_block(uint8_t address, uint8_t reg, uint8_t length);
 
 #endif /* I2C_h */
