3bc4df9dac9d15dc1e6c498b7d4662d2bb842a4a
Samuel Dewan <samdewan@me.com>
2017-11-05 00:14:16 -0400

    Modified ADC code to take the average of a configurable number of samples each time it reads each channel.

--- CU-in-Space-2018-Avionics-Software/ADC.c
+++ CU-in-Space-2018-Avionics-Software/ADC.c
@@ -10,16 +10,25 @@
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// MARK: Constants
+#define ADC_FLAG_AUTO_ENABLED   2   // When this flag is set to one the ADC will take readings at a set interval
+#define ADC_FLAG_VALUE_CURRENT  1   // When this flag is set the values in adc_avg_data are up to date
+#define ADC_FLAG_IN_PROGRESS    0   // This flag is set to one when there is a set of conversions taking place
+
 // MARK: Variable Definitions
-uint16_t adc_data[ADC_NUM_CHANNELS];
+static uint16_t adc_raw_data[ADC_NUM_CHANNELS][ADC_NUM_SAMPLES];
+uint16_t adc_avg_data[ADC_NUM_CHANNELS];
 
-volatile uint8_t adc_enable_mask;
-volatile uint8_t adc_flags;
+/** Various boolean fields used by the ADC*/
+static volatile uint8_t adc_flags;
 
-uint32_t adc_last_sample_time;
+/** The time in milliseconds since startup that the last read of the ADC was started*/
+static uint32_t adc_last_sample_time;
 
 /** The next channel to be read from the ADC*/
 static volatile uint8_t adc_current_chan;
+/** The current sample number*/
+static volatile uint8_t adc_sample_num;
 
 // MARK: Function Definitions
 
@@ -35,7 +44,21 @@ void init_adc(void)
 
 void adc_service(void)
 {
-    if ((adc_flags & (1<<ADC_FLAG_AUTO_ENABLED)) && (millis - adc_last_sample_time) >= ADC_AUTO_PERIOD) {
+    uint8_t adc_active = adc_flags & (1<<ADC_FLAG_IN_PROGRESS);
+    
+    if (!(adc_flags & (1<<ADC_FLAG_VALUE_CURRENT)) && !adc_active) {
+        // Find average value for each channel
+        for (int i = 0; i < ADC_NUM_CHANNELS; i++) {
+            uint16_t sample_sum = 0;
+            for (int j =0; j < ADC_NUM_SAMPLES; i++) {
+                sample_sum += (uint16_t) adc_raw_data[i][j];
+            }
+            adc_avg_data[i] = (uint8_t)(sample_sum / ADC_NUM_SAMPLES);
+        }
+        adc_flags |= (1<<ADC_FLAG_VALUE_CURRENT);
+    }
+    
+    if (!adc_active && (adc_flags & (1<<ADC_FLAG_AUTO_ENABLED)) && (millis - adc_last_sample_time) >= ADC_AUTO_PERIOD) {
         // Start a new set of conversions
         adc_last_sample_time = millis;
         adc_start_conversion();
@@ -48,6 +71,7 @@ void adc_start_conversion(void)
         return;
     }
     adc_flags |= (1<<ADC_FLAG_IN_PROGRESS);
+    adc_flags &= ~(1<<ADC_FLAG_VALUE_CURRENT);
     
     // Select next enabled channel
     uint8_t adc_chan_selected = 0;
@@ -61,6 +85,8 @@ void adc_start_conversion(void)
     }
     
     if (adc_chan_selected) {            // We don't want to bother turning on the ADC if no channels are enabled
+        adc_sample_num = 0;
+        
         PRR0 &= ~(1<<PRADC);            // Turn on power to the ADC
         ADCSRA |= (1<<ADEN);            // Enable the ADC
         ADMUX |= adc_current_chan;      // Set adc channel
@@ -74,23 +100,33 @@ ISR(ADC_vect)   // ADC Conversion Complete
     uint16_t value = ADCL;
     value |= (((uint16_t)ADCH) << 8);
     
-    adc_data[adc_current_chan] = value;
+    adc_raw_data[adc_current_chan][adc_sample_num] = value;
     
-    // Select the next enabled channel
-    uint8_t adc_chan_selected = 0;
-    for (uint8_t i = adc_current_chan + 1; i < ADC_NUM_CHANNELS; i++) {
-        if (ADC_ENABLE_MASK & (1<<i)) {
-            adc_chan_selected = 1;
-            adc_current_chan = i;
-            ADMUX |= adc_current_chan;  // Set adc channel
-            ADCSRA |= (1<<ADSC);        // Start the next ADC conversion
-            break;
+    adc_sample_num++;
+    if (adc_sample_num < ADC_NUM_SAMPLES) {
+        // Take next sample
+        ADCSRA |= (1<<ADSC);                // Start the next ADC conversion
+    } else {
+        // Switch to next channel
+        adc_sample_num = 0;
+        
+        // Select the next enabled channel
+        uint8_t adc_chan_selected = 0;
+        for (uint8_t i = adc_current_chan + 1; i < ADC_NUM_CHANNELS; i++) {
+            if (ADC_ENABLE_MASK & (1<<i)) {
+                adc_chan_selected = 1;
+                adc_current_chan = i;
+                ADMUX |= adc_current_chan;  // Set adc channel
+                ADCSRA |= (1<<ADSC);        // Start the next ADC conversion
+                break;
+            }
         }
-    }
-    
-    if (!adc_chan_selected) {
-        adc_flags &= ~(1<<ADC_FLAG_IN_PROGRESS);
-        ADCSRA &= ~(1<<ADEN);           // Disable the ADC
-        PRR0 |= (1<<PRADC);             // Shut down ADC to save power
+        
+        if (!adc_chan_selected) {
+            // finished conversion
+            adc_flags &= ~(1<<ADC_FLAG_IN_PROGRESS);
+            ADCSRA &= ~(1<<ADEN);           // Disable the ADC
+            PRR0 |= (1<<PRADC);             // Shut down ADC to save power
+        }
     }
 }
--- CU-in-Space-2018-Avionics-Software/ADC.h
+++ CU-in-Space-2018-Avionics-Software/ADC.h
@@ -13,23 +13,16 @@
 #include "global.h"
 
 // MARK: Constants
-#define ADC_NUM_CHANNELS        8       // Number of ADC channels aviable. This includeds channels which are not being used
-#define ADC_AUTO_PERIOD         4000    // The period at which readings will be taken from all enabled channels
-
-#define ADC_FLAG_AUTO_ENABLED   0   // When this flag is set to one the ADC will take readings at a set interval
-#define ADC_FLAG_IN_PROGRESS    1   // This flag is set to one when there is a set of conversions taking place
+#define ADC_NUM_CHANNELS    8       // Number of ADC channels aviable, this includeds channels which are not being used
+#define ADC_NUM_SAMPLES     10      // The number of samples to average for each reading
+#define ADC_AUTO_PERIOD     4000    // The period at which readings will be taken from all enabled channels in milliseconds
 
 // Setting a bit in DIDR0 will enable reading on the corresponding analog input and disable the corresponding digital pin
-#define ADC_ENABLE_MASK         DIDR0
+#define ADC_ENABLE_MASK     DIDR0
 
 // MARK: Variable Declarations
 /** The most resently read data from each ADC*/
-extern uint16_t adc_data[ADC_NUM_CHANNELS];
-
-/** Various boolean fields used by the ADC*/
-extern volatile uint8_t adc_flags;
-/** The time in milliseconds since startup that the last read of the ADC was started*/
-extern uint32_t adc_last_sample_time;
+extern uint16_t adc_avg_data[ADC_NUM_CHANNELS];
 
 // MARK: Function Prototypes
 
