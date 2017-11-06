be9f7caa08eac12f338051ca60ac165f0d87c9c5
Samuel Dewan <samdewan@me.com>
2017-11-06 15:11:40 -0500

    Redesigned I2C implementation to queue transactions.

--- CU-in-Space-2018-Avionics-Software/I2C.c
+++ CU-in-Space-2018-Avionics-Software/I2C.c
@@ -7,40 +7,44 @@
 
 #include "I2C.h"
 
+#include <stddef.h> //NULL
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
+// MARK: Type Definitions
+typedef struct {
+    uint8_t     transaction_id;     // A unique identifier for this transaction
+    
+    uint8_t     slave_address;      // The 7-bit address for the peripheral
+    uint8_t     slave_register;     // The register address to be read or written
+    uint8_t     num_bytes;          // The number of bytes to be read or written from the peripheral register
+    uint8_t     *buffer;            // The buffer which data is sent from or recieved data is placed in
+    
+    uint8_t     position;           // The current position within the transaction
+    uint8_t     error_count;        // The number of errors which have occured during the current transaction
+    
+    uint8_t     write:1;            // If this bit is set this transaction is a write operation, otherwise a read operation
+    uint8_t     active:1;           // This bit is set when the transaction is currently on the bus
+    uint8_t     successful:1;       // This bit is set if the transaction is complete and did not fail
+    uint8_t     done:1;             // This bit if set if the transaction is complete, regardless of success
+} i2c_transaction_t;
+
 // MARK: Constants
-#define I2C_WRITE_BIT   0
-#define I2C_READ_BIT    1
+#define I2C_MAX_ERRORS          3   // Number of errors before an I2C interaction is aborted
 
-#define I2C_MAX_ERRORS  3     // Number of errors before an I2C interaction is aborted
+#define I2C_TRANSACTION_VOID    0   // The ID of an invalid transaction
+#define I2C_TRANSACTION_FIRST   1   // The first valid transaction ID
 
-#define I2C_FLAGS_ACTIVE            0
-#define I2C_FLAGS_SUCCESSFUL        1
+#define I2C_NUM_TRANSACTIONS    8   // The maximum number of transactions which can be buffered
 
 // MARK: Variable Definitions
-/** The buffer into which the data recieved in the current transaction is written*/
-static uint8_t *i2c_rx_buffer;
-/** The buffer from which the data sent in current transaction is read*/
-static uint8_t *i2c_tx_buffer;
-/** A temporary buffer to be used internaly for transactions wehre the data to be sent is passed as a function argument*/
-static uint16_t i2c_short_tx_data_buffer;
 
-/** The address of the slave in the current transaction*/
-static volatile uint8_t i2c_address;
-/** The address of the slave register which is being accesed during the current transaciton*/
-static volatile uint8_t i2c_reg;
-/** The number of bytes to be send in the current transaction after the start byte*/
-static volatile uint8_t i2c_tx_length;
-/** The number of bytes to be recieved in the current transaction after all bytes are sent*/
-static volatile uint8_t i2c_rx_length;
-/** Position within the current transaction*/
-static volatile uint8_t i2c_position;
-/** Number of retrys which have occured during the current transaction*/
-static volatile uint8_t i2c_error_count;
-/** Various boolean fields for the i2c interface*/
-static volatile uint8_t i2c_flags;
+/** The buffer of i2c transactions*/
+i2c_transaction_t i2c_transactions[I2C_NUM_TRANSACTIONS];
+/** The address to be given to the next transaction on the i2c bus*/
+uint8_t i2c_next_transaction;
+
+uint8_t i2c_buffer_position;
 
 // MARK: Function Definitions
 void init_i2c(void)
@@ -48,46 +52,146 @@ void init_i2c(void)
     
 }
 
+/**
+ *  Determine the next pending transaction and start transmitting it.
+ *  Does nothing if there is already an active transaction or if there are no pending transactions.
+ */
+void i2c_start_next_transaction (void)
+{
+    if (i2c_transactions[i2c_buffer_position].active) {
+        return;
+    }
+    
+    uint8_t i = 0;
+    do {
+        if ((i2c_transactions[i].transaction_id != I2C_TRANSACTION_VOID) && (!i2c_transactions[i].done)) {
+            i2c_buffer_position = i;
+            // start i2c_transactions[i]
+            return;
+        }
+        i = ((i + 1) < I2C_NUM_TRANSACTIONS) ? i + 1 : 0;
+    } while (i != i2c_buffer_position);
+}
+
 void i2c_service(void)
 {
-    
+    i2c_start_next_transaction();
 }
 
-uint8_t i2c_transfer_active(void)
+/**
+ *  Get a transaction with a certain ID
+ *  @param transaction_id The id of the transaction which should be retrieved
+ *  @return A pointer the transaction with an in matching transaction_id. Null if there is no such transaction.
+ */
+i2c_transaction_t *get_transaction (uint8_t transaction_id)
 {
-    return 1;
+    if (transaction_id != I2C_TRANSACTION_VOID) {
+        for (int i = 0; i < I2C_NUM_TRANSACTIONS; i++) {
+            if (i2c_transactions[i].transaction_id == transaction_id) {
+                return &i2c_transactions[i];
+            }
+        }
+    }
+    return NULL;
 }
 
-
-void i2c_write_byte(uint8_t address, uint8_t reg, uint8_t data)
+uint8_t i2c_transfer_done(uint8_t transaction_id)
 {
-    
+    i2c_transaction_t *trans = get_transaction(transaction_id);
+    if (trans != NULL) {
+        return 0;
+    } else {
+        return trans->done;
+    }
 }
 
-void i2c_write_word(uint8_t address, uint8_t reg, uint16_t data)
+uint8_t i2c_transfer_successful(uint8_t transaction_id)
 {
-    
+    i2c_transaction_t *trans = get_transaction(transaction_id);
+    if (trans != NULL) {
+        return 0;
+    } else {
+        return trans->successful;
+    }
 }
 
-void i2c_write_block(uint8_t address, uint8_t reg, uint8_t *data, uint8_t length)
-{
+/**
+ *  Gets a pointer to the next free slot in the transaction buffer
+ *  @return A pointer to the next free transaction or NULL if there are no free transactions
+ */
+i2c_transaction_t *get_next_free_transaction(void) {
+    uint8_t i = i2c_buffer_position;
     
+    do {
+        if (i2c_transactions[i].transaction_id == I2C_TRANSACTION_VOID) {
+            return &i2c_transactions[i];
+        }
+        i = ((i + 1) < I2C_NUM_TRANSACTIONS) ? i + 1 : 0;
+    } while (i != i2c_buffer_position);
+    
+    return NULL;
 }
 
-
-void i2c_read_byte(uint8_t address, uint8_t reg, uint8_t *data)
+uint8_t i2c_write(uint8_t *transaction_id, uint8_t address, uint8_t reg, uint8_t *data, uint8_t length)
 {
+    i2c_transaction_t *trans = get_next_free_transaction();
+    if (trans == NULL) {
+        return 1;
+    }
     
+    uint8_t next = ((i2c_next_transaction + 1) == I2C_TRANSACTION_VOID) ? I2C_TRANSACTION_FIRST : i2c_next_transaction + 1;
+    
+    trans->transaction_id = i2c_next_transaction;
+    *transaction_id = i2c_next_transaction;
+    i2c_next_transaction = next;
+    
+    trans->slave_address = address;
+    trans->slave_register = reg;
+    trans->buffer = data;
+    trans->num_bytes = length;
+    
+    trans->position = 0;
+    trans->error_count = 0;
+    
+    trans->write = 1;
+    trans->active = 0;
+    trans->successful = 0;
+    trans->done = 0;
+    
+    i2c_start_next_transaction();
+    
+    return 0;
 }
 
-void i2c_read_word(uint8_t address, uint8_t reg, uint16_t *data)
+uint8_t i2c_read(uint8_t *transaction_id, uint8_t address, uint8_t reg, uint8_t *data, uint8_t length)
 {
+    i2c_transaction_t *trans = get_next_free_transaction();
+    if (trans == NULL) {
+        return 1;
+    }
     
-}
-
-void i2c_read_block(uint8_t address, uint8_t reg, uint8_t *data, uint8_t length)
-{
+    uint8_t next = ((i2c_next_transaction + 1) == I2C_TRANSACTION_VOID) ? I2C_TRANSACTION_FIRST : i2c_next_transaction + 1;
     
+    trans->transaction_id = i2c_next_transaction;
+    *transaction_id = i2c_next_transaction;
+    i2c_next_transaction = next;
+    
+    trans->slave_address = address;
+    trans->slave_register = reg;
+    trans->buffer = data;
+    trans->num_bytes = length;
+    
+    trans->position = 0;
+    trans->error_count = 0;
+    
+    trans->write = 0;
+    trans->active = 0;
+    trans->successful = 0;
+    trans->done = 0;
+    
+    i2c_start_next_transaction();
+    
+    return 0;
 }
 
 // MARK: Interupt service routines
--- CU-in-Space-2018-Avionics-Software/I2C.h
+++ CU-in-Space-2018-Avionics-Software/I2C.h
@@ -21,65 +21,40 @@ extern void init_i2c(void);
 extern void i2c_service(void);
 
 /**
- *  Determine if there is a transfer in progress on the i2c bus
- *  @returns 1 if there is an active transfer on the i2c bus, otherwise returns 0
+ *  Check if an i2c transfer has finished
+ *  @param transaction_id The transaction to check
+ *  @return A non zero value if the transaction has finished
  */
-extern uint8_t i2c_transfer_active(void);
-
-
-/**
- *  Writes a byte to a register via the i2c bus
- *  @param address The address of the slave to which the byte should be sent
- *  @param reg The address of the register to which the data should be written
- *  @param data The byte to be sent
- */
-extern void i2c_write_byte(uint8_t address, uint8_t reg, uint8_t data);
+extern uint8_t i2c_transfer_done(uint8_t transaction_id);
 
 /**
- *  Writes a word to a register via the i2c bus
- *  @param address The address of the slave to which the byte should be sent
- *  @param reg The address of the register to which the data should be written
- *  @param data The word to be sent
+ *  Check if an i2c transfer was successful
+ *  @param transaction_id The transaction to check
+ *  @return A non zero value if the transaction was successful
  */
-extern void i2c_write_word(uint8_t address, uint8_t reg, uint16_t data);
+extern uint8_t i2c_transfer_successful(uint8_t transaction_id);
 
 /**
- *  Writes a block to a register via the i2c bus
- *  @param address The address of the slave to which the byte should be sent
- *  @param reg The address of the register to which the data should be written
- *  @param data An array of bytes to be sent
- *  @param length The number of bytes to be sent
+ *  Adds a write transaction to the queue
+ *  @param transaction_id The transaction id will be placed at this address
+ *  @param address The address of the slave peripheral
+ *  @param reg The address of the register to be written
+ *  @param data The data to be written
+ *  @param length The number of byte to be written
+ *  @return Zero if the transaction was sucessfully created
  */
-extern void i2c_write_block(uint8_t address, uint8_t reg, uint8_t *data, uint8_t length);
+extern uint8_t i2c_write(uint8_t *transaction_id, uint8_t address, uint8_t reg, uint8_t *data, uint8_t length);
 
 /**
- *  Read a one byte wide register via the i2c bus
- *  The received byte will be written to the i2c bus RX buffer
- *  @param address The address of the slave with which the transfer should be completed
- *  @param reg The address of the register which should be read
- *  @param data The memory where the response should be stored, must be at least one byte long
- *
- *  @note The
+ *  Adds a read transaction to the queue
+ *  @param transaction_id The transaction id will be placed at this address
+ *  @param address The address of the slave peripheral
+ *  @param reg The address of the register to be read
+ *  @param data A buffer in which to place the data from the peripheral
+ *  @param length The number of byte to be read
+ *  @return Zero if the transaction was sucessfully created
  */
-extern void i2c_read_byte(uint8_t address, uint8_t reg, uint8_t *data);
-
-/**
- *  Read one word wide a register via the i2c bus
- *  The received byte will be written to the i2c bus RX buffer
- *  @param address The address of the slave with which the transfer should be completed
- *  @param reg The address of the register which should be read
- *  @param data The memory where the response should be stored, must be at least two bytes long
- */
-extern void i2c_read_word(uint8_t address, uint8_t reg, uint16_t *data);
-
-/**
- *  Read a register via the i2c bus
- *  The received byte will be written to the i2c bus RX buffer
- *  @param address The address of the slave with which the transfer should be completed
- *  @param reg The address of the register which should be read
- *  @param length The number of bytes to be read from the peripheral
- *  @param data The memory where the response should be stored, must be at least length bytes long
- */
-extern void i2c_read_block(uint8_t address, uint8_t reg, uint8_t *data, uint8_t length);
+extern uint8_t i2c_read(uint8_t *transaction_id, uint8_t address, uint8_t reg, uint8_t *data, uint8_t length);
 
 #endif /* I2C_h */
+
