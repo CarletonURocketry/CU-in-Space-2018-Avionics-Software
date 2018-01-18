2c8cec00794660b5580a7d4e3923b5f45d299d45
Samuel Dewan <samdewan@me.com>
2018-01-18 12:19:07 -0500

    Wrote header files for sensors

--- CU-in-Space-2018-Avionics-Software/ADXL343.h
+++ CU-in-Space-2018-Avionics-Software/ADXL343.h
@@ -9,4 +9,40 @@
 #ifndef ADXL343_h
 #define ADXL343_h
 
+#include "global.h"
+
+// MARK: Constants
+#define ADXL343_SAMPLE_RATE   5   // The number of times per second that the sensor should be polled
+
+// MARK: Variables
+/**
+ *  The value of the global millis variable when the last sample was recieved from the sensor
+ */
+extern uint32_t adxl343_sample_time;
+
+/**
+ *  The acceleration in the x axis as recieved from the sensor
+ */
+extern int16_t adxl343_accel_x;
+/**
+ *  The acceleration in the y axis as recieved from the sensor
+ */
+extern int16_t adxl343_accel_y;
+/**
+ *  The acceleration in the z axis as recieved from the sensor
+ */
+extern int16_t adxl343_accel_z;
+
+// MARK: Function declarations
+/**
+ *  Initilize the ADXL343 3-axis accelerometer
+ *  @returns 0 if the function was successfull
+ */
+extern uint8_t init_adxl343(void);
+
+/**
+ *  Code to be run in each iteration of the main loop
+ */
+extern void adxl343_service(void);
+
 #endif /* ADXL343_h */
--- CU-in-Space-2018-Avionics-Software/FGPMMOPA6H.h
+++ CU-in-Space-2018-Avionics-Software/FGPMMOPA6H.h
@@ -9,6 +9,50 @@
 #ifndef FGPMMOPA6H_h
 #define FGPMMOPA6H_h
 
-#include <stdio.h>
+#include "global.h"
+
+// MARK: Constants
+#define FGPMMOPA6H_SAMPLE_RATE   5   // The number of times per second that the sensor should be polled
+
+// MARK: Variables
+/**
+ *  The value of the global millis variable when the last sample was recieved from the sensor
+ */
+extern uint32_t fgpmmopa6h_sample_time;
+
+/**
+ *  The UTC time according to the GPS module. See Telemetery Format document for more information.
+ */
+extern uint32_t fgpmmopa6h_utc_time;
+
+/**
+ *  The lattitude according to the GPS module. See Telemetery Format document for more information.
+ */
+extern int32_t fgpmmopa6h_lattitude;
+/**
+ *  The longitude according to the GPS module. See Telemetery Format document for more information.
+ */
+extern int32_t fgpmmopa6h_longitude;
+
+/**
+ *  The land speed according to the GPS module in hundredths of a knot
+ */
+extern int16_t fgpmmopa6h_speed;
+/**
+ *  The course according to the GPS module in hundredths of a degree
+ */
+extern int16_t fgpmmopa6h_course;
+
+// MARK: Function declarations
+/**
+ *  Initilize the FGPMMOPA6H GPS
+ *  @returns 0 if the function was successfull
+ */
+extern uint8_t init_fgpmmopa6h(void);
+
+/**
+ *  Code to be run in each iteration of the main loop
+ */
+extern void fgpmmopa6h_service(void);
 
 #endif /* FGPMMOPA6H_h */
